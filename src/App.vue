af9f06a047b69b5cc7e799ba9eccbf7bc3162320
yangyankang <704799639@qq.com>
2018-04-19 16:18:58 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <header-comp></header-comp>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import headerComp from './components/header/header'
+
+export default {
+  name: 'App',
+  components: {
+    headerComp
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    .tab {
+      display: flex;
+      height: 40px;
+      line-height: 40px;
+      .tab-item {
+        text-align: center;
+        flex: 1;
+      }
+    }
+  }
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+    这是头部
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-header'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
